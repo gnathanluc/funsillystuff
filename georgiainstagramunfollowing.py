feb6d85bfd99d087f8e3d29ffe472669295de02b
gnathanluc <150853173+gnathanluc@users.noreply.github.com>
2024-02-09 14:20:52 -0600

    Add files via upload

---
+++ georgiainstagramunfollowing.py
@@ -0,0 +1,11 @@
+'''
+georgia instagram unfollowing code
+2/9/24
+'''
+echo "# funsillystuff" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/gnathanluc/funsillystuff.git
+git push -u origin main
